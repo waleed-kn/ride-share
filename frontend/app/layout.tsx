a0b441d6d5b3ab6e08396d3b5b0377c7a637e92c
waleed-kn <furiouswaleed.99@gmail.com>
2026-09-24 18:45:10 +0500

    Map Box is added

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { AuthProvider } from "@/lib/auth";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export const metadata: Metadata = {
   title: "RideShare",
